08161d5a72aef4203238476c495ccad1600120b4
25550td-Thinh <25550td@hvhs.school.nz>
2026-06-15 21:47:03 +0000

    Jun 15, 2026, 9:47 PM

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Hello teacher!")
+console.log("My name is Thinh")
+
+// This is a single line comment
+
+//Main code
+
+
+
+
+
+//Functions
