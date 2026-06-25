f9f0f54d3d4d19e823d39e0c3adea6377568f808
25550td-Thinh <25550td@hvhs.school.nz>
2026-06-25 00:57:04 +0000

    Jun 25, 2026, 12:57 AM

--- script.js
+++ script.js
@@ -29,10 +29,18 @@ let quantity= 0;
 
 //Functions
 function customerName(){
+    if(name === customerName){
     const name= document.getElementById("customerName").value;
     console.log(name);
     Output(name);
+    if(name === ""){
+        alert("Please enter your name!");
+        return false;
+    } else {
+        document.getElementById("orderForm").style.display= "block";
+    }
 }
 function Output(name){
-    document.getElementById("output").innerHTML= "<p>Hello " + name + "! Welcome to our cafe!</p>";
+    document.getElementById("output").innerHTML= "<p>Hello " + name + "! Welcome to our cafe!You are ready to order.</p>";
+}
 }
