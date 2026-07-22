17d0914eff427eaec981038dbb5ca3a4d7c9e4db
25550td-Thinh <25550td@hvhs.school.nz>
2026-07-22 00:05:32 +0000

    Jul 22, 2026, 12:05 AM

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function placeOrder() {
    document.getElementById("output").innerHTML = "Please enter your name.";
     return;
   }
-  if (money<total) {
-    document.getElementById("output").innerHTML = "You dont have enough money.";
+  if (money>total) {
+    document.getElementById("output").innerHTML = "Thank you for your order, " + name + "! Your total is " + money + ".";
   }
 }
